e6c2d08b21b1c2c4927adc5b6aaa192bd513e7bf
Billy Flaherty <billyflaherty@Williams-MacBook-Pro.local>
2015-05-18 11:13:46 -0400

    Some cleanup and additional comments to explain config.

--- config.example.php
+++ config.example.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Copy/rename this file to config.php then update the values to match your local environment.
+ */
 
 $_CONFIG = [
         'backup_path'         => '/path/to/files',
@@ -7,7 +10,7 @@ $_CONFIG = [
         'DB_USER'             => 'root',
         'DB_PASS'             => '',
         'DB_NAME'             => 'photos-db',
-        'MAX_UPLOAD_ATTEMPTS' => 0, // 0 = unlimited
+        'MAX_UPLOAD_ATTEMPTS' => 0, // The max number of files to upload during script execution. ( 0 = unlimited )
         'aws_autoloader'      => '/path/to/aws/aws-autoloader.php', // AWS php
         'aws_bucket'          => 'your-aws-bucket-name',
         'ok_extensions'       => ['jpg', 'jpeg', 'png', 'gif', 'mov', '3gp', 'avi', 'mpg']
